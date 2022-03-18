c824982b0168ef5ada342f395a5d9d0e34fa80b2
GraniteMask <ratnadeepastronomy@gmail.com>
2022-03-18 10:57:27 +0530

    guess the number

--- guess_the_number.java
+++ guess_the_number.java
@@ -2,6 +2,15 @@ package com.company;
 
 public class guess_the_number {
     public static void main(String[] args){
-        
+      /*
+          Create a class game, which allows a user to play "Guess the number" game once. Game should have the
+          following methods:
+
+          1. Constructor to generate the random number
+          2. takeUserInput() to take a user input of number
+          3. isCorrectNumber() to detect whether the number entered by the user is true
+          4. getter and setter for noOfGuesses
+          Use properties such as noOfGuesses(int), etc to get this task done!
+       */
     }
 }
