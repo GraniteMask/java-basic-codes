156b1cd90214f3bb3c6a5980857054be9e153d15
GraniteMask <ratnadeepastronomy@gmail.com>
2022-03-20 02:07:52 +0530

    inheritance

--- constructors_in_Inheritance.java
+++ constructors_in_Inheritance.java
@@ -3,6 +3,10 @@ package com.company;
 class Base1{
     public int x;
 
+    Base1(){
+        System.out.println("I am a constructor");
+    }
+
     public void setX(int x) {
         this.x = x;
     }
@@ -26,6 +30,6 @@ class Derived1 extends Base1{
 
 public class constructors_in_Inheritance {
     public static void main(String[] args){
-
+        Base1 b = new Base1();
     }
 }
