f1138ecc77002f54ba76462b0aec239e92002ec9
GraniteMask <ratnadeepastronomy@gmail.com>
2022-03-20 02:13:23 +0530

    constructors in inheritance

--- constructors_in_Inheritance.java
+++ constructors_in_Inheritance.java
@@ -7,6 +7,10 @@ class Base1{
         System.out.println("I am a constructor");
     }
 
+    Base1(int a){
+        System.out.println("Constructor:" + a);
+    }
+
     public void setX(int x) {
         this.x = x;
     }
@@ -26,10 +30,16 @@ class Derived1 extends Base1{
     public int getY() {
         return y;
     }
+
+    Derived1(){
+        super(0);
+        System.out.println("I am a Derived class constructor");
+    }
 }
 
 public class constructors_in_Inheritance {
     public static void main(String[] args){
-        Base1 b = new Base1();
+//        Base1 b = new Base1();
+        Derived1 d = new Derived1();
     }
 }
