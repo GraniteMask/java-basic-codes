ec790c249aa77410979189add3f1bd86cb10b8a8
GraniteMask <ratnadeepastronomy@gmail.com>
2022-03-20 02:04:10 +0530

    inheritance

---
+++ constructors_in_Inheritance.java
@@ -0,0 +1,19 @@
+package com.company;
+
+class Base{
+    public int x;
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+}
+
+public class constructors_in_Inheritance {
+    public static void main(String[] args){
+
+    }
+}
