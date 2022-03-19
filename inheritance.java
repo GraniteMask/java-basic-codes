43d28ecaf3bcfcecfc59bf325f513cdf13b3fc84
GraniteMask <ratnadeepastronomy@gmail.com>
2022-03-20 01:49:40 +0530

    inheritance

---
+++ inheritance.java
@@ -0,0 +1,16 @@
+package com.company;
+
+class Base{
+    int x;
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+}
+
+public class inheritance {
+}
