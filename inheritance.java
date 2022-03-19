0d916a0e0047fd551ce14d07aa7cffce344f3ea8
GraniteMask <ratnadeepastronomy@gmail.com>
2022-03-20 01:53:53 +0530

    inheritance

--- inheritance.java
+++ inheritance.java
@@ -10,7 +10,28 @@ class Base{
     public int getX() {
         return x;
     }
+
+    public void printMe(){
+        System.out.println("I am a constructor");
+    }
+}
+
+class Derived extends Base{
+    int y;
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
 
 public class inheritance {
+    public static void main(String[] args){
+        Base b = new Base();
+        b.setX(4);
+        System.out.println(b.getX());
+    }
 }
