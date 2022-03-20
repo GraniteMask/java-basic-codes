9ca14e3dc1d0d70d85f3f7c3957bd30027fdef1a
GraniteMask <ratnadeepastronomy@gmail.com>
2022-03-20 19:53:43 +0530

    interfaces

--- interfaces.java
+++ interfaces.java
@@ -7,7 +7,13 @@ interface bicycle{
 
 class AvonCycle implements bicycle{
     void blowHorn(){
-        
+        System.out.println("Blow Horn");
+    }
+    public void applyBrake(int decrement){
+        System.out.println("Applying Brake");
+    }
+    public void speedUp(int increment){
+        System.out.println("Applying Speedup");
     }
 }
 
