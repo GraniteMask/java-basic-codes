71aa24cbad050ee9819b001e2b22d530173e65e0
GraniteMask <ratnadeepastronomy@gmail.com>
2022-03-20 19:48:58 +0530

    interfaces

---
+++ interfaces.java
@@ -0,0 +1,15 @@
+package com.company;
+
+interface bicycle{
+    void applyBrake(int decrement);
+    void speedUp(int increment);
+}
+
+class AvonCycle implements bicycle{
+    void blowHorn(){
+        
+    }
+}
+
+public class interfaces {
+}
