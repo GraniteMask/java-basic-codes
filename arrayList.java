d58416a3f2b92484a55cc10e1bae2daeda18f17d
GraniteMask <ratnadeepastronomy@gmail.com>
2022-04-13 23:14:13 +0530

    arrayalist

--- arrayList.java
+++ arrayList.java
@@ -4,7 +4,20 @@ import java.util.*;
 public class arrayList {
     public static void main(String[] args){
         ArrayList<Integer> l1 = new ArrayList<>();
+        ArrayList<Integer> l2 = new ArrayList<>();
+        l2.add(15);
+        l2.add(18);
+        l2.add(19);
+
         l1.add(6);
-        
+        l1.add(7);
+        l1.add(4);
+        l1.add(6);
+        l1.add(0,5);
+        l1.add(0,1);    // since this is added later so it will be at the top then the element will be 5
+        l1.addAll(l2);   // adding all the elements of l2 in l1
+        for(int i=0; i<l1.size(); i++){
+            System.out.println(l1.get(i)); // getting the value of ith element
+        }
     }
 }
