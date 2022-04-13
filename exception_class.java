2cf29d3b675905255e55a42e8970aefaeb980dd6
GraniteMask <ratnadeepastronomy@gmail.com>
2022-04-13 19:38:13 +0530

    exception class

--- exception_class.java
+++ exception_class.java
@@ -5,7 +5,11 @@ class MyException extends Exception{
     public String toString(){
         return super.toString() + " I am toString()";
     }
-    
+
+    @Override
+    public String getMessage() {
+        return super.getMessage() + " I am getMessage()";
+    }
 }
 
 public class exception_class {
