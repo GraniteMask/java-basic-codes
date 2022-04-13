b9b96c2debff872950254e078f11bd071a9cbcc6
GraniteMask <ratnadeepastronomy@gmail.com>
2022-04-13 21:13:09 +0530

    exception class

--- throw_and_throws.java
+++ throw_and_throws.java
@@ -1,8 +1,23 @@
 package com.company;
 
+class NegativeRadiusException extends Exception{
+    @Override
+    public String toString() {
+        return "Radius cannot be negative!";          // write toString then press enter to get the auto suggestion for this whole block
+    }
+
+    @Override
+    public String getMessage() {
+        return "Radius cannot be negative";
+    }
+}
+
 public class throw_and_throws {
-    public static float area(int r){
-        float result = (float) (Math.PI*r*r);
+    public static double area(int r){
+        if(r<0){
+            throw NegativeRadiusException;
+        }
+        double result = (Math.PI*r*r);
         return result;
     }
     public static int divide(int a, int b) throws ArithmeticException{
