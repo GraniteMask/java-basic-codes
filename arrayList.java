abed42a7da2eee9c8124030d4f242eff1d697b11
GraniteMask <ratnadeepastronomy@gmail.com>
2022-04-13 23:19:07 +0530

    arrayList

--- arrayList.java
+++ arrayList.java
@@ -17,6 +17,7 @@ public class arrayList {
         l1.add(0,1);    // since this is added later so it will be at the top then the element will be 5
         l1.addAll(l2);   // adding all the elements of l2 in l1
         System.out.println(l1.contains(27));    // return boolean after checking whether this number is in the arraylist or not
+        System.out.println(l1.indexOf(27));      // return the index number of that number. If that number is not available then it returns -1.
 //        l1.clear();
         for(int i=0; i<l1.size(); i++){
             System.out.println(l1.get(i)); // getting the value of ith element
