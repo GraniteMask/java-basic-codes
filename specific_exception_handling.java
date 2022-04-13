53559c632b4433648757d6ddc5ce50fabd0db786
GraniteMask <ratnadeepastronomy@gmail.com>
2022-04-13 11:22:11 +0530

    specific error handling

---
+++ specific_exception_handling.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class specific_exception_handling {
+    public static void main(String[] args){
+        int [] marks = new int[3];
+        marks[0] = 7;
+        marks[1] = 56;
+        marks[1] = 6;
+        Scanner sc= new Scanner(System.in);
+
+    }
+}
