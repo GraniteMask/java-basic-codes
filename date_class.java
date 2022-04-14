d5b27fa00a2f2060156645bfbc7addedaa5ec657
GraniteMask <ratnadeepastronomy@gmail.com>
2022-04-14 20:32:17 +0530

    hashSet

---
+++ date_class.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Date;
+
+public class date_class {
+    public static void main(String[] args) {
+//        System.out.println(Long.MAX_VALUE);
+//        System.out.println(System.currentTimeMillis());
+        Date d = new Date();
+        System.out.println(d);
+        System.out.println(d.getTime());
+    }
+}
