5a60fdae85b16781e153cf836ccca2a91b5cfa1f
GraniteMask <ratnadeepastronomy@gmail.com>
2022-04-14 20:19:27 +0530

    hashSet

--- hashSet_implementation.java
+++ hashSet_implementation.java
@@ -4,6 +4,10 @@ import java.util.HashSet;
 
 public class hashSet_implementation {
     public static void main(String[] args) {
-        HashSet<Integer> myHashSet = new HashSet<>();
+        HashSet<Integer> myHashSet = new HashSet<>(6,0.5f);
+        myHashSet.add(6);
+        myHashSet.add(8);
+        myHashSet.add(3);
+        myHashSet.add(11);
     }
 }
