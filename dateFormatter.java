d2c80f0aaf76fbb98117cc2d0c8d2bc011a12029
GraniteMask <ratnadeepastronomy@gmail.com>
2022-04-15 18:41:18 +0530

    Date Time formatter

---
+++ dateFormatter.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.time.LocalDateTime;
+
+public class dateFormatter {
+    public static void main(String[] args) {
+        LocalDateTime dt = LocalDateTime.now();
+        System.out.println(dt);
+    }
+}
